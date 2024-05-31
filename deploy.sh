d55db3a5e62d2e7265d796cef991b96e9297674c
ULStudent:OLUWAJOMILOJU.OLAJITAN <23373326@studentmail.ul.ie>
2024-05-31 21:28:05 +0100

    deploy file changed

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
-if ! command -v aws &> /dev/null
-then
-    echo "AWS CLI not found installing..."
-    pip install awscli
-fi
+echo "Deploying to AWS..."
 
-echo "Deployinh to S3..."
-aws s3 sync ./dist s3://webbingbucket --delete/
+aws s3 cp ./dist s3://your-bucket-name --recursive
+
